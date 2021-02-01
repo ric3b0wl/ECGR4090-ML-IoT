2145640e64d4ddc49bdb30e45f377a79fc785b93
Sam Xu <shengkaixu123@gmail.com>
2021-02-01 12:07:24 -0500

    Plotted

--- hw1_python_basics/hw1p1.py
+++ hw1_python_basics/hw1p1.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# pip install numpy matplotlib
+# conda install numpy matplotlib
+
 import person
 
 #=====================================================================================
@@ -25,4 +31,27 @@ people = {}
 for i in range(len(list_of_names)):
 	people[list_of_names[i]] = person.person(name = list_of_names[i], age = list_of_ages[i], height = list_of_heights_cm[i])
 
-print(people)
+# print(people)
+
+
+# numpy as np
+ary_age = np.array(list_of_ages)
+ary_height_cm = np.array(list_of_heights_cm)
+
+age_avg = np.mean(list_of_ages)  # ().mean -> average function(Python statistics)
+height_avg = np.mean(ary_height_cm)
+
+print (f"the average age is: {age_avg}")
+print (f"the average height is: {height_avg}")
+
+
+# matplotlib as plt
+plt.scatter(ary_age, ary_height_cm)
+plt.grid(True)
+
+plt.xlabel('age')
+plt.ylabel('hgith in cm')
+plt.title("age vs height(cm)")
+
+plt.savefig("./age-vs-height_plot.jpg")
+plt.show()
